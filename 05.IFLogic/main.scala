1a8e9e2d4b5a2d6c6b0cb845191f3efcde8e4b28
Sarin CR <sarinrays@gmail.com>
2021-09-06 22:09:50 +0530

    if

---
+++ 05.IFLogic/main.scala
@@ -0,0 +1,11 @@
+object Demo {
+   def main(args: Array[String]) {
+      var x = 30;
+
+      if( x < 20 ){
+         println("X< 20");
+      } else {
+         println("X >20");
+      }
+   }
+}
