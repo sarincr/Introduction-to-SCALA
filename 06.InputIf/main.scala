3e9944561dbdbf70ea2d1cb7e62164fe9b826eab
Sarin CR <sarinrays@gmail.com>
2021-11-01 19:53:12 +0530

    Input and IF

---
+++ 06.InputIf/main.scala
@@ -0,0 +1,9 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+        println ("Enter Age : ") 
+        var age:Int = scala.io.StdIn.readInt() 
+        if(age > 18){  
+        println ("Age is greate than 18")  
+    }  
+  }
+}
